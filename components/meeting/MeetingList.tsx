d2eb81968e2120f02a7e06cd923b166380f8deee
Pour1a-D <pour1a.dra80@gmail.com>
2024-06-26 20:57:48 +0330

    added toast and create meeting method

--- components/meeting/MeetingList.tsx
+++ components/meeting/MeetingList.tsx
@@ -1,10 +1,13 @@
 "use client";
 
-import { useState } from "react";
-import { useRouter } from "next/navigation";
-
 import MeetingModal from "./MeetingModal";
 import HomeShortcutCard from "../HomeShortcutCard";
+
+import { useState } from "react";
+import { useUser } from "@clerk/nextjs";
+import { useToast } from "../ui/use-toast";
+import { useRouter } from "next/navigation";
+import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk";
 
 const initialValues = {
     dateTime: new Date(),
@@ -13,7 +16,14 @@ const initialValues = {
 };
 
 const MeetingList = () => {
+    const { user } = useUser();
+    const { toast } = useToast();
+
     const router = useRouter();
+    const client = useStreamVideoClient();
+
+    const [callDetail, setCallDetail] = useState<Call>();
+    const [values, setValues] = useState(initialValues);
 
     const [meetingState, setMeetingState] = useState<
         | "isScheduleMeeting"
@@ -22,9 +32,47 @@ const MeetingList = () => {
         | undefined
     >(undefined);
 
-    const [values, setValues] = useState(initialValues);
+    const createMeeting = async () => {
+        if (!client || !user) return;
 
-    const createMeeting = () => {};
+        try {
+            if (!values.dateTime) {
+                toast({ title: "Please select a date and time" });
+                return;
+            }
+
+            const id = crypto.randomUUID();
+            const call = client.call("default", id);
+
+            if (!call) throw new Error("Failed to create meeting");
+
+            const startsAt =
+                values.dateTime.toISOString() ||
+                new Date(Date.now()).toISOString();
+
+            const description = values.description || "Instant Meeting";
+
+            await call.getOrCreate({
+                data: {
+                    starts_at: startsAt,
+                    custom: {
+                        description,
+                    },
+                },
+            });
+
+            setCallDetail(call);
+            if (!values.description) {
+                router.push(`/meeting/${call.id}`);
+            }
+            toast({
+                title: "Meeting Created",
+            });
+        } catch (error: any) {
+            console.log(error);
+            toast({ title: "Failed to create Meeting" });
+        }
+    };
 
     return (
         <section className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-5">
