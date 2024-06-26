e9e3cced102acd9d1ef592320118ec2c11d1cdfa
Pour1a-D <pour1a.dra80@gmail.com>
2024-06-26 13:11:41 +0330

    project created

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
-  plugins: {
-    tailwindcss: {},
-  },
+    plugins: {
+        "postcss-import": {},
+        "tailwindcss/nesting": {},
+        tailwindcss: {},
+        autoprefixer: {},
+    },
 };
 
 export default config;
