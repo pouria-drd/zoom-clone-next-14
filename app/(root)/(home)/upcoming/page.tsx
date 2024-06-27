854476fe487f553d1b0ba2ad3e96d57b873a1b50
Pour1a-D <pour1a.dra80@gmail.com>
2024-06-27 11:19:02 +0330

    addedd useGetCalls, meeting card and call list, upcoming page done

--- app/(root)/(home)/upcoming/page.tsx
+++ app/(root)/(home)/upcoming/page.tsx
@@ -1,7 +1,11 @@
+import CallList from "@/components/meeting/CallList";
+
 const UpcomingPage = () => {
     return (
         <section className="text-white flex flex-col gap-10 size-full">
             <h1 className="text-3xl font-bold">UpcomingPage</h1>
+
+            <CallList type="upcoming" />
         </section>
     );
 };
